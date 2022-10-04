8daa6fb67768946455820ea9232d86166e6d0176
Qingwen Xiao <parkerciao@gmail.com>
2022-10-04 15:35:50 +1000

    XML dependency injection for null attribute and special symbols

--- src/com/parker/spring5/Book.java
+++ src/com/parker/spring5/Book.java
@@ -5,16 +5,15 @@ public class Book {
 
 
     private String bname;
+    private String author;
 
     public void setBname(String bname) {
         this.bname = bname;
     }
 
-    public static void main(String[] args){
-        Book book = new Book();
-        book.setBname("abc");
+    public void setAuthor(String author) {
+        this.author = author;
     }
-
 
     public void testDemo(){
         System.out.println(bname);
